c4e6a4b342dd3b8252d0f90828b83a23715cebf9
Tomoya Nagasawa <ocrybit@gmail.com>
2024-01-30 14:49:13 +0400

    [circuit-json] $contains_all $contains_none

--- circom/json/gen2.js
+++ circom/json/gen2.js
@@ -4,9 +4,9 @@ const gen2 = async ({ size_val = 5, size_path = 5, size_json = 256 }) => {
   const doc = new Doc({ size_val, size_path, size_json })
   return {
     inputs: await doc.getInputs({
-      json: { c: [1, 2, 3], b: 3 },
+      json: { c: [1, 2, 3, false], b: 3 },
       path: "c",
-      query: ["$contains_any", [0, 4, 1]],
+      query: ["$contains_none", [4, 5, 6, true]],
     }),
   }
 }
