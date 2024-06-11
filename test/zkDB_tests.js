135939195971b7c5874932fb347cf05ab79a0b11
eludius18 <eladio.robcas@gmail.com>
2024-06-11 10:41:04 +0100

    initial setup for MongoDB connection

--- test/zkDB_tests.js
+++ test/zkDB_tests.js
@@ -1,4 +1,5 @@
-const { resolve } = require("path")
+const { resolve } = require("path");
+const MongoClient = require('mongodb').MongoClient;
 const {
   path,
   val,
@@ -14,7 +15,7 @@ const {
   encodeQuery,
   decodeQuery,
   DB,
-} = require("../sdk")
+} = require("../sdk");
 const {
   insert,
   slice,
@@ -36,45 +37,60 @@ const {
   remove,
   bn,
   digits,
-} = require("../sdk/uint")
-const { parse } = require("../sdk/parse")
-const { expect } = require("chai")
-const { groth16 } = require("snarkjs")
+} = require("../sdk/uint");
+const { parse } = require("../sdk/parse");
+const { expect } = require("chai");
+const { groth16 } = require("snarkjs");
+
+require('dotenv').config({ path: resolve(__dirname, '../.env') });
 
 describe("zkDB-zkJSON", function () {
-  this.timeout(0)
+  this.timeout(0);
   it.only("should generate proofs", async () => {
+    const url = process.env.MONGO_URL;
+    const dbName = process.env.MONGO_DB;
+    const client = new MongoClient(url);
+    await client.connect();
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+
     const wasm = resolve(
       __dirname,
-      "../circom/build/circuits/db/index_js/index.wasm",
-    )
+      "../circom/build/circuits/db/index_js/index.wasm"
+    );
     const zkey = resolve(
       __dirname,
-      "../circom/build/circuits/db/index_0001.zkey",
-    )
+      "../circom/build/circuits/db/index_0001.zkey"
+    );
 
-    const zkdb = new DB({ wasm, zkey })
-    await zkdb.init()
-    await zkdb.addCollection()
-    await zkdb.insert(0, "Bob", { gamer: "Bob", strikes: 30, place: "New York" })
+    const jsonInfo = { gamer: "John", strikes: 40, place: "NY" };
+
+    const zkdb = new DB({ wasm, zkey });
+    await zkdb.init();
+    await zkdb.addCollection();
+    await zkdb.insert(0, jsonInfo.gamer, jsonInfo);
     const zkp = await zkdb.genProof({
-      json: { gamer: "Bob", strikes: 30, place: "New York" },
+      json: jsonInfo,
       col_id: 0,
-      path: "gamer",
-      id: "Bob",
-    })
-
-    await zkdb.addCollection()
-    await zkdb.insert(1, "Alice", { name: "Alice" })
-    const zkp2 = await zkdb.genProof({
-      json: { name: "Alice" },
-      col_id: 1,
       path: "name",
-      id: "Alice",
-    })
+      id: jsonInfo.gamer,
+    });
+
+    const finalJson = { ...jsonInfo, zkProof: zkp };
+
+    const collection1 = db.collection('counterstrike');
+    await collection1.insertOne(finalJson);
+
+    //const collection2 = db.collection('collection2');
+    //await collection2.insertOne({ name: "Alice" });
+    //const zkp2 = await zkdb.genProof({
+    //  json: { name: "Alice" },
+    //  col_id: 1,
+    //  path: "name",
+    //  id: "Alice",
+    //})
 
     //console.log("zkp1", zkp)
     //console.log("zkp2", zkp2)
-  })
-  
-})
+  });
+});
